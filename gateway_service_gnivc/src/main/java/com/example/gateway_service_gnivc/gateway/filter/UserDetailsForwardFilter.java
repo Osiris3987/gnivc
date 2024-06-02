8a66c2484fb72b7ce65b8e1235d8ae93fa1ae36c
Oleg Lomakin <yfnf72@gmail.com>
2024-06-03 00:35:31 +0300

    added userDetailForwardFilter

--- gateway_service_gnivc/src/main/java/com/example/gateway_service_gnivc/gateway/filter/UserDetailsForwardFilter.java
+++ gateway_service_gnivc/src/main/java/com/example/gateway_service_gnivc/gateway/filter/UserDetailsForwardFilter.java
@@ -1,2 +1,39 @@
-package com.example.gateway_service_gnivc.gateway.filter;public class UserDetailsForwardFilter {
+package com.example.gateway_service_gnivc.gateway.filter;
+
+import com.example.gateway_service_gnivc.security.model.UserDetailsImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class UserDetailsForwardFilter implements GlobalFilter {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return ReactiveSecurityContextHolder.getContext()
+                .filter(c -> c.getAuthentication() != null)
+                .flatMap(c -> {
+                    UserDetailsImpl userDetails = (UserDetailsImpl) c.getAuthentication().getPrincipal();
+                    ServerHttpRequest request = getModifiedRequest(exchange, userDetails);
+                    return chain.filter(exchange.mutate().request(request).build());
+                })
+                .switchIfEmpty(chain.filter(exchange));
+    }
+
+    @SneakyThrows
+    private ServerHttpRequest getModifiedRequest(ServerWebExchange exchange, UserDetailsImpl userDetails) {
+        return exchange.getRequest().mutate()
+                .header("x-userId", userDetails.getUserId())
+                .header("x-roles", objectMapper.writeValueAsString(userDetails.getRoles()))
+                .build();
+    }
 }
