94ab66c6d834b6af97fc61a8a190da8e0d8bf323
Oleg Lomakin <yfnf72@gmail.com>
2024-06-13 18:09:19 +0300

    added endpoint for posting event images

--- driver_service/src/main/java/com/example/driver_service/web/controller/ImageController.java
+++ driver_service/src/main/java/com/example/driver_service/web/controller/ImageController.java
@@ -1,2 +1,21 @@
-package com.example.driver_service.web.controller;public class ImageController {
+package com.example.driver_service.web.controller;
+
+import com.example.driver_service.config.feign.ImageFeignClient;
+import com.example.driver_service.web.dto.RaceEventImage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/images")
+@RequiredArgsConstructor
+public class ImageController {
+
+    private final ImageFeignClient imageClient;
+
+    @PostMapping
+    public String createImage(RaceEventImage image) {
+        return imageClient.createRaceEventImage(image);
+    }
 }
--- driver_service/src/main/java/com/example/driver_service/web/dto/RaceEventImage.java
+++ driver_service/src/main/java/com/example/driver_service/web/dto/RaceEventImage.java
@@ -1,12 +1,14 @@
-package com.example.dwh_service.model;
+package com.example.driver_service.web.dto;
 
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class RaceEventImage {
+public class RaceEventImage implements Serializable {
 
     private MultipartFile file;
 }
